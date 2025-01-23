8b8351df79a540fe262c876d91c61186fcfef259
Jaroslav Fowkes <jaroslav.fowkes@stfc.ac.uk>
2025-01-23 10:17:14 +0000

    Add serial wavefield precond test

--- test/accelerate_tests/base_tests/engine_tests.py
+++ test/accelerate_tests/base_tests/engine_tests.py
@@ -68,13 +68,13 @@ class MLSerialTest(unittest.TestCase):
         RMSE_ob = (np.mean(np.abs(OBJ_ML_serial - OBJ_ML)**2))
         RMSE_pr = (np.mean(np.abs(PRB_ML_serial - PRB_ML)**2))
         # RMSE_LL = (np.mean(np.abs(LL_ML_serial - LL_ML)**2))
-        np.testing.assert_allclose(RMSE_ob, 0.0, atol=1e-2, 
+        np.testing.assert_allclose(RMSE_ob, 0.0, atol=1e-2,
                                     err_msg="The object arrays are not matching as expected")
-        np.testing.assert_allclose(RMSE_pr, 0.0, atol=1e-2, 
+        np.testing.assert_allclose(RMSE_pr, 0.0, atol=1e-2,
                                     err_msg="The object arrays are not matching as expected")
         # np.testing.assert_allclose(RMSE_LL, 0.0, atol=1e-7,
         #                             err_msg="The log-likelihood errors are not matching as expected")
-    
+
 
     def test_ML_serial_base(self):
         out = []
@@ -146,6 +146,25 @@ class MLSerialTest(unittest.TestCase):
             engine_params.smooth_gradient = 20
             engine_params.smooth_gradient_decay = 1/10.
             engine_params.scale_precond = False
+            out.append(tu.EngineTestRunner(engine_params, output_path=self.outpath, init_correct_probe=True,
+                                           scanmodel="BlockFull", autosave=False, verbose_level="critical"))
+        self.check_engine_output(out, plotting=False, debug=False)
+
+    def test_ML_serial_wavevefield_preconditioner(self):
+        out = []
+        for eng in ["ML", "ML_serial"]:
+            engine_params = u.Param()
+            engine_params.name = eng
+            engine_params.numiter = 100
+            engine_params.floating_intensities = False
+            engine_params.reg_del2 = False
+            engine_params.reg_del2_amplitude = 1.
+            engine_params.smooth_gradient = 0.
+            engine_params.smooth_gradient_decay = 0.
+            engine_params.scale_precond = False
+            engine_params.wavefield_precond = True
+            engine_params.wavefield_delta_object = 0.1
+            engine_params.wavefield_delta_probe = 0.1
             out.append(tu.EngineTestRunner(engine_params, output_path=self.outpath, init_correct_probe=True,
                                            scanmodel="BlockFull", autosave=False, verbose_level="critical"))
         self.check_engine_output(out, plotting=False, debug=False)
